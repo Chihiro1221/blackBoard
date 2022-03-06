1630cc8959bfdc013c28cd5fbed57b752f4d751c
luca <2213595911@qq.com>
2022-03-06 15:27:18 +0800

    添加橡皮擦和截图功能

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ class Board {
   ) {
     this.initCanvas()
     this.bindEvnet()
+    this.draw()
   }
 
   // 初始化画布
@@ -27,8 +28,11 @@ class Board {
     const btn = document.createElement('button')
     btn.innerText = '清屏'
     btn.addEventListener('click', () => {
-      this.app.fillStyle = '#000'
+      this.bgColor = '#000'
+      this.app.fillStyle = this.bgColor
       this.app.fillRect(0, 0, this.width, this.height)
+      this.recoverLineWidth(1)
+      this.lineColor = 'white'
     })
     this.btns.insertAdjacentElement('beforeend', btn)
 
@@ -50,18 +54,32 @@ class Board {
     container.classList.add('colors-container')
     container.innerText = '画笔颜色:'
     // 渲染按钮
-    this.colors.forEach(color => {
+    this.colors.forEach((color, index) => {
       const div = document.createElement('div')
+      if (index === 0) {
+        div.classList.add('active')
+        this.lineColor = color
+      }
       div.classList.add('color')
       div.style.backgroundColor = color
       container.insertAdjacentElement('beforeend', div)
 
       div.addEventListener('click', () => {
+        this.removeClass('active')
+        div.classList.add('active')
         this.lineColor = color
       })
     })
 
     this.btns.insertAdjacentElement('beforeend', container)
+
+    return this
+  }
+
+  private removeClass(className: string) {
+    const colors = document.querySelectorAll('.color')
+    colors.forEach(div => div.classList.remove(className))
+    return this
   }
 
   // 绑定事件
@@ -83,6 +101,55 @@ class Board {
     this.app.lineTo(event.offsetX, event.offsetY)
     this.app.stroke()
   }
+
+  // 橡皮
+  public erase() {
+    const btn = document.createElement('button')
+    btn.innerText = '橡皮擦'
+    btn.addEventListener('click', () => {
+      this.lineColor = this.bgColor
+      this.recoverLineWidth(10)
+    })
+    this.btns.insertAdjacentElement('beforeend', btn)
+    return this
+  }
+
+  // 修改画笔大小
+  private recoverLineWidth(width: number) {
+    this.app.lineWidth = width
+  }
+
+  // 恢复画笔
+  public draw() {
+    const btn = document.createElement('button')
+    btn.innerText = '画笔'
+    this.recoverLineWidth(1)
+    btn.addEventListener('click', () => {
+      this.lineColor = 'white'
+      this.recoverLineWidth(1)
+    })
+    this.btns.insertAdjacentElement('beforeend', btn)
+    return this
+  }
+
+  // 截图
+  public short() {
+    const btn = document.createElement('button')
+    btn.innerText = '截图'
+    btn.addEventListener('click', () => {
+      const img = document.querySelector<HTMLImageElement>('.canvas-image')
+      if (img) {
+        img.src = this.el.toDataURL('image/jpeg')
+        return this
+      }
+      const image = document.createElement('img')!
+      image.classList.add('canvas-image')
+      image.src = this.el.toDataURL('image/jpeg')
+      this.btns.insertAdjacentElement('afterend', image)
+    })
+    this.btns.insertAdjacentElement('beforeend', btn)
+    return this
+  }
 }
 
-const instance = new Board().clear().setBgColor('#9b59b6').setLineColor()
+const instance = new Board().clear().erase().short().setLineColor()
