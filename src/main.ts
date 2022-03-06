78d957e2f4f9c80d02e82ccaa2503851678072f4
luca <2213595911@qq.com>
2022-03-06 14:19:45 +0800

    添加清屏功能

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ class Board {
     private el = document.querySelector<HTMLCanvasElement>('#canvas')!,
     private width = el.width,
     private height = el.height,
-    private app = el.getContext('2d')!
+    private app = el.getContext('2d')!,
+    private btns: HTMLDivElement = document.createElement('div'),
+    private bgColor = '#000',
+    private lineColor = '#fff'
   ) {
     this.initCanvas()
     this.bindEvnet()
@@ -12,8 +15,31 @@ class Board {
 
   // 初始化画布
   private initCanvas() {
-    this.app.fillStyle = '#000'
+    this.app.fillStyle = this.bgColor
     this.app.fillRect(0, 0, this.width, this.height)
+    this.btns.style.cssText = 'margin:10px'
+    this.el.insertAdjacentElement('afterend', this.btns)
+  }
+
+  // 清屏
+  public clear() {
+    const btn = document.createElement('button')
+    btn.innerText = '清屏'
+    btn.addEventListener('click', () => {
+      this.app.fillRect(0, 0, this.width, this.height)
+    })
+    this.btns.insertAdjacentElement('beforeend', btn)
+
+    return this
+  }
+
+  // 设置画布颜色
+  public setBgColor(color: string) {
+    this.bgColor = color
+    this.app.fillStyle = color
+    this.app.fillRect(0, 0, this.width, this.height)
+
+    return this
   }
 
   // 绑定事件
@@ -21,13 +47,12 @@ class Board {
     const callback = this.drawLine.bind(this)
     this.el.addEventListener('mousedown', () => {
       this.app.beginPath()
-      this.app.strokeStyle = 'white'
+      this.app.strokeStyle = this.lineColor
       this.el.addEventListener('mousemove', callback)
-
-      // 移除事件
-      document.addEventListener('mouseup', () => {
-        this.el.removeEventListener('mousemove', callback)
-      })
+    })
+    // 移除事件
+    document.addEventListener('mouseup', () => {
+      this.el.removeEventListener('mousemove', callback)
     })
   }
 
@@ -38,4 +63,4 @@ class Board {
   }
 }
 
-const instance = new Board()
+const instance = new Board().clear().setBgColor('#9b59b6')
